93f17b0c633501485e505d6ed96dd80acefff7d8
NeroStark <nerostarktv@gmail.com>
2013-08-15 19:10:15 +0200

    Remove tests

--- test/helpers/invoice_payments_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class InvoicePaymentsHelperTest < ActionView::TestCase
-end
--- test/helpers/payment_rules_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class PaymentRulesHelperTest < ActionView::TestCase
-end
