5e55727f23faaddb6614b772793988c81dde541f
Xavier Dutreilh <xavier@dutreilh.fr>
2013-08-17 10:51:27 +0200

    Display authentication information in account details

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,10 @@ module ApplicationHelper
       value.present? ? value : 'Not available'
   end
 
+  def display_datetime(datetime)
+    datetime.present? ? l(value) : 'Not available'
+  end
+
   def display_mail_to(address, name = nil)
     address.present? ? mail_to(address, (name if name.present?)) : 'Not available'
   end
