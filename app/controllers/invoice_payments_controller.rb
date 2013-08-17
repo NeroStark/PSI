7fc91d4b4e0baafe36fc82e5d7422711f42c78d3
Xavier Dutreilh <xavier@dutreilh.fr>
2013-08-17 10:40:42 +0200

    Add JSON format to /invoice_payments/:id/(un)pay

--- app/controllers/invoice_payments_controller.rb
+++ app/controllers/invoice_payments_controller.rb
@@ -4,19 +4,27 @@ class InvoicePaymentsController < ApplicationController
 
   # GET /invoice_payments/1/pay
   def pay
-    if @invoice_payment.update_attributes(paid: true)
-      redirect_to root_path, notice: 'Invoice has been successfully marked as paid.'
-    else
-      redirect_to root_path, alert: 'Invoice cannot be marked as paid.'
+    respond_to do |format|
+      if @invoice_payment.update_attributes(paid: true)
+        format.html { redirect_to root_path, notice: 'Invoice has been successfully marked as paid.' }
+        format.json { head :no_content }
+      else
+        format.html { redirect_to root_path, alert: 'Invoice cannot be marked as paid.' }
+        format.json { render json: @invoice_payment.errors, status: :unprocessable_entity }
+      end
     end
   end
 
   # GET /invoice_payments/1/unpay
   def unpay
-    if @invoice_payment.update_attributes(paid: false)
-      redirect_to root_path, notice: 'Invoice has been successfully marked as unpaid.'
-    else
-      redirect_to root_path, alert: 'Invoice cannot be marked as unpaid.'
+    respond_to do |format|
+      if @invoice_payment.update_attributes(paid: false)
+        format.html { redirect_to root_path, notice: 'Invoice has been successfully marked as unpaid.' }
+        format.json { head :no_content }
+      else
+        format.html { redirect_to root_path, alert: 'Invoice cannot be marked as unpaid.' }
+        format.json { render json: @invoice_payment.errors, status: :unprocessable_entity }
+      end
     end
   end
 
